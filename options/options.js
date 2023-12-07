c3b850f34be1071b03eaf1c12f1ebbc6a3e2b6fe
Crowdbotics <team@crowdbotics.com>
2023-12-07 17:25:41 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "voice-over-44889",
+  url: "https://voice-over-44889.botics.co",
+  api: "https://voice-over-44889.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
